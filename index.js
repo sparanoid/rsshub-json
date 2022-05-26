ce868ed9a39c1ba55ee86c240417e136d9d75c83
Tunghsiao Liu <t@sparanoid.com>
2022-05-26 15:15:40 -0400

    feat: init repo

---
+++ index.js
@@ -0,0 +1,33 @@
+import RSSHub from 'rsshub';
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+RSSHub.init({
+  // config
+});
+
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+  res.json({
+    name: 'rsshub-json',
+    status: 'ok'
+  });
+});
+
+app.get('/*', (req, res, next) => {
+  RSSHub.request(req.url)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(e => {
+      next(e);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`rsshub-json listening on port ${port}`)
+});
